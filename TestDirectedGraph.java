8aa7d8d829a6248a53aa9ebdb546322d24c33305
zheng <2451752823@qq.com>
2025-05-28 20:10:35 +0800

    black test

--- TestDirectedGraph.java
+++ TestDirectedGraph.java
@@ -3,6 +3,7 @@ import java.lang.reflect.Field;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class TestDirectedGraph {
   private DirectedGraph graph;
 
@@ -19,6 +20,7 @@ public class TestDirectedGraph {
     field.setAccessible(true);
     field.set(obj, value);
   }
+
 
   @Test
   @DisplayName("测试桥接词生成")
